e27dd3959811199cdd685d694c21a02c66d1c0fe
Charlie Lin <hlin266@uwo.ca>
2018-06-06 15:13:59 -0400

    Added Edit and Update Features

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require ("express"),
 app = express(),
 bodyParser = require("body-parser"),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+methodOverride = require("method-override");
 
 mongoose.connect("mongodb://charlielin:test123@ds251210.mlab.com:51210/restful_blog_test", { useMongoClient: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -57,6 +59,29 @@ app.get("/blogs/:id", function(req,res){
 			res.redirect("/blogs");
 		} else{
 			res.render("show", {blog: foundBlog})		
+		}
+	})
+})
+
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else{
+			res.render("edit", {blog: foundBlog});
+		}
+	})
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if (err){
+			res.redirect("/blogs");
+		} else{
+			res.redirect("/blogs/" + req.params.id);
 		}
 	})
 })
