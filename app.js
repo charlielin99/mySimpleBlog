752f7315147ba9528c41db86dfad51691f2606b3
Charlie Lin <hlin266@uwo.ca>
2018-06-07 17:08:56 -0400

    Completed UI Changes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require ("express"),
 app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
-methodOverride = require("method-override");
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer");
 
-mongoose.connect("mongodb://charlielin:test123@ds251210.mlab.com:51210/restful_blog_test", { useMongoClient: true });
+mongoose.connect("mongodb://charlielin:test123@ds251210.mlab.com:51210/restful_blog_test");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
@@ -42,6 +44,7 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE ROUTE
 app.post("/blogs", function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			res.render("new");
@@ -77,6 +80,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if (err){
 			res.redirect("/blogs");
